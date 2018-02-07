31096710ff7d3651271954460d0f5611a26df295
Mario vizcaino <mario.vizcainos@gmail.com>
2018-02-07 09:42:47 -0500

    added app filter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import posts from './posts'
-
 
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.handleOnChange = this.handleOnChange.bind(this)
+    this.state = {posts: posts}
+  }
+  handleOnChange (event) {
+    let updatedList = posts
+    updatedList = updatedList.filter(item => {
+      return item.title.toLowerCase().search(
+        event.target.value.toLowerCase()
+      ) !== -1
+    })
+    this.setState({posts: updatedList})
+  }
+  render () {
     return (
       <div>
-        <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+        <div className='filter'>
+          <input type='text' placeholder='Ingresa el término de búsqueda' onChange={this.handleOnChange} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {this.state.posts.map((post, index) => {
+            return (
+              <li key={index}>
+                <a href={post.url}><img src={post.image} alt={post.title} /></a>
+                <p>{ post.title }</p>
+              </li>
+            )
+          })}
+
         </ul>
       </div>
     )
   }
 }
 
-
 export default App
-
-
